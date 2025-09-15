48689be86a15177bf219b6e9cd940c7b0a2bf44e
septiandwica <septiandwica03@gmail.com>
2025-09-15 10:42:59 +0700

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const morgan = require('morgan');
-require('dotenv').config();
+const express = require("express");
+const morgan = require("morgan");
+const database_pool = require("./config/db");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,10 +9,20 @@ const app = express();
 
 app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+database_pool
+  .getConnection()
+  .then((connection_db) => {
+    console.log("Connected to the MySQL database");
+    connection_db.release();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
